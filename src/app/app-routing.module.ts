72585c31950df612a251946e59aa1d4c5096b71f
Savitha Kumari R <skumarir@in.ibm.com>
2020-05-06 16:35:48 +0530

    new entries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ExploreComponent } from './explore/explore.component';
 
 
 const routes: Routes = [
+  
   {
-    path:'home',
-    component:HomeComponent
+    path:'login',
+    component:LoginComponent
+  },
+
+  {
+    path:'explore',
+    component:ExploreComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+import { ExploreComponent } from './explore/explore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
